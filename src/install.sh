894f01a113ef8e7a6f8c5f347e19f8e8f8b62bbd
gushi-cookie <gushi-cookie@protonmail.com>
2025-03-14 18:08:01 +0000

    Improved the code. Created new bats-core tests.

--- src/install.sh
+++ src/install.sh
@@ -2,12 +2,23 @@
 set -e
 
 # Used environments:
-# 	BASH_CRUD_DOWNLOADER:
+# 	BASH_CRUD_DOWNLOADER
 #			Which network command should be used for
 #			downloading files and making http requests.
 #			Valid values: ["curl", "wget"]. Default: "curl".
-#
-# 	TO-DO - desc
+#   BC_TEST__PASS_COMMAND
+#     A space-separated list of command names that
+#     should be treated as an existing commands for
+#     testing purposes. Used by the has_cmd() function.
+#   BC_TEST__MISS_COMMAND
+#     A space-separated list of command names that
+#     should be treated as missing commands for testing
+#   	purposes. Used by the has_cmd() function.
+
+
+# = = = = = = = = = =
+#     Utilities
+# = = = = = = = = = =
 
 print_error() {
 	# Print an error message to the stderr stream.
@@ -17,7 +28,6 @@ print_error() {
 
 	printf "%s\\n" "$*" >&2
 }
-
 
 print_error_and_exit() {
 	# Print an error message to the stderr stream and
@@ -30,7 +40,6 @@ print_error_and_exit() {
 	return 1
 }
 
-
 get_current_version_tag() {
 	# Get a hardcoded version tag of the currently
 	# supported or developed version.
@@ -38,6 +47,21 @@ get_current_version_tag() {
 	printf "v0.1.0"
 }
 
+includes_by_delimiter() {
+	# Check if a list of strictly delimited values
+	# has an item with an exact value.
+	# Arguments:
+	#   $1 - The list of delimited values.
+	#   $2 - The delimiter.
+	#   $3 - The value to check.
+	# Returns:
+	#   0 - If the item is included.
+	#   1 - If the item is not included.
+
+
+	[[ " $(echo "$1" | tr "$2" " ") " == *" ${3} "* ]]
+	return $?
+}
 
 has_cmd() {
 	# Check if a command is available on the current system.
@@ -47,10 +71,70 @@ has_cmd() {
 	# 	0 - The command exists.
 	# 	1 - The command does not exist.
 
+
+	if includes_by_delimiter "${BC_TEST__PASS_COMMAND:-}" " " "$1"; then
+		return 0
+	elif includes_by_delimiter "${BC_TEST__MISS_COMMAND:-}" " " "$1"; then
+		return 1
+	fi
+
 	type "$1" > /dev/null 2>&1
 }
 
+
+# = = = = = = = = = = = = = = = =
+#    Managing: Required tools
+# = = = = = = = = = = = = = = = =
+
+get_architecture_for_jq() {
+	# Get an architecture suffix for the 'jqlang/jq' tool,
+	# according to its ci.yml file and the current machine's
+	# architecture.
+	# Conditions:
+	#		The script exits with an error if the machine's
+	#		architecture not supported/listed.
+	# Returns:
+	# 	An architecture suffix for the 'jq' tool.
+
+
+	case "$(uname -m)" in
+				 'x86_64') printf 'amd64';;
+		'i686'|'i386') printf 'i386';;
+				'aarch64') printf 'arm64';;
+					's390x') printf 's390x';;
+				'riscv64') printf 'riscv64';;
+								*) print_error_and_exit "Architecture '$(uname -m)' for 'jq' tool not supported.";;
+	esac
+}
+
+establish_jq() {
+	# Setup the 'jq' tool if it is not installed.
+
+	if has_cmd "jq"; then return 0; fi
+	printf "Command 'jq' not found. Downloading it for the current session..\n"
+
+	local temp_path
+	temp_path="$(establish_temp_path)"
+
+	download_file "$temp_path/jq" "$(get_download_link "jq")"
+	chmod 111 "$temp_path/jq"
+}
+
 get_downloader() {
+	# Get a command name of the supported tool for
+	# making http requests.
+	# Environments:
+	# 	BASH_CRUD_DOWNLOADER - force to use a specific command.
+	#		Valid values are 'curl' and 'wget'.
+	# Conditions:
+	# 	If BASH_CRUD_DOWNLOADER is invalid or the supplied
+	#		command not found - exits with an error.
+	#		If none of supported commands not found - exits
+	#		with an error.
+	# Returns(by print):
+	# 	The appropriate downloader command name.
+
+
 	if [ "${BASH_CRUD_DOWNLOADER:-}" == "curl" ]; then
 		if has_cmd "curl"; then printf %s "curl"; return; fi
 		print_error_and_exit "Selected downloader command 'curl' not found on the system."
@@ -71,6 +155,10 @@ get_downloader() {
 }
 
 
+# = = = = = = = = = = = =
+#    Networking: Http
+# = = = = = = = = = = = =
+
 download_file() {
 	# Download a file using curl or wget. Query parameters
 	# may be passed as arguments after supplying required arguments.
@@ -79,15 +167,11 @@ download_file() {
 	# 	$1 - The name of the output file that may include an absolute path.
 	# 	$2 - The URL of the request.
 	#		$Q - The list of query parameters for the request.
-	# Conditions:
-	# 	The script exits with an error if the commands are not found.
 	# Examples:
 	# 	1) download_file /dev/null https..com "req=5" "delete=yes" "please=sir"
 
 
-	local downloader
-	downloader="$(get_downloader)"
-
+	local downloader=get_downloader
 	local outfile="$1"
 	local url="$2"
 	shift 2
@@ -118,19 +202,15 @@ make_get_request() {
 	# may be passed as arguments after supplying required arguments.
 	# They get url encoded automatically.
 	# Arguments:
-	# 	$2 - The URL of the request.
+	# 	$1 - The URL of the request.
 	#		$Q - The list of query parameters for the request.
-	# Conditions:
-	# 	The script exits with an error if the commands are not found.
 	# Examples:
 	# 	1) download_file /dev/null https..com "req=5" "delete=yes please=sir"
 	# Returns(by print):
 	# 	The received response content.
 
 
-	local downloader
-	downloader="$(get_downloader)"
-
+	local downloader=get_downloader
 	local url="$1"
 	shift
 
@@ -155,99 +235,6 @@ make_get_request() {
 	fi
 }
 
-declare -r temp_path="/tmp/bash-crud"
-
-
-establish_temp_path() {
-	# Prepare a directory in '/tmp' for temporal files
-	# of the current session and add that path to the
-	# PATH variable.
-
-	if [ ! -d "$temp_path" ]; then
-		mkdir -p "$temp_path"
-	fi
-
-	if [[ ! "$PATH" =~ $temp_path ]]; then
-		PATH="$PATH:$temp_path"
-	fi
-}
-
-
-declare -r default_awkpath="/usr/share/awk"
-
-
-establish_gawk_path() {
-	# Prepare a child directory for gawk programs
-	# according to a value of the AWKPATH variable.
-	# Conditions:
-	# 	If AWKPATH has multiple paths then the last one is used.
-	# 	If AWKPATH is unset then 'default_awkpath' is used.
-	# Returns(by print):
-	#		A directory path for gawk programs.
-
-
-	local awk_path
-	awk_path="$(gawk 'BEGIN { len=split(ENVIRON["AWKPATH"], arr, ":"); printf "%s", arr[len] }')"
-	awk_path="${awk_path:-$default_awkpath}"
-
-	local path="${awk_path}/bash-crud"
-	mkdir -p "$path"
-	printf %s "$path"
-}
-
-
-get_bin_dir() {
-	if [ -n "${BASH_CRUD_BIN_DIR:-}" ]; then
-		printf %s "$BASH_CRUD_BIN_DIR"
-	else
-		printf "/usr/local/bin"
-	fi
-}
-
-
-# = = =
-#  jq
-# = = =
-
-
-get_architecture_for_jq() {
-	# Get an architecture suffix for the 'jqlang/jq' tool,
-	# according to its ci.yml file and the current machine's
-	# architecture.
-	# Conditions:
-	#		The script exits with an error if the machine's
-	#		architecture not supported/listed.
-	# Returns:
-	# 	An architecture suffix for the 'jq' tool.
-
-
-	case "$(uname -m)" in
-				 'x86_64') printf 'amd64';;
-		'i686'|'i386') printf 'i386';;
-				'aarch64') printf 'arm64';;
-					's390x') printf 's390x';;
-				'riscv64') printf 'riscv64';;
-								*) print_error_and_exit "Architecture '$(uname -m)' for 'jq' tool not supported.";;
-	esac
-}
-
-
-establish_jq() {
-	# Setup the 'jq' tool if it is not installed.
-
-	if has_cmd "jq"; then return; fi
-
-	printf "Command 'jq' not found. Downloading it for the current session.."
-	establish_temp_path
-	download_file "$temp_path/jq" "$(get_download_link "jq")"
-	chmod 111 "$temp_path/jq"
-}
-
-
-# = = = = = = = = =
-#  Download links
-# = = = = = = = = =
-
 get_file_links_from_github_repo() {
 	# Make a github API request to retrieve the content
 	# of a specified repository's directory. Then extract
@@ -265,6 +252,66 @@ get_file_links_from_github_repo() {
 	printf ""
 }
 
+
+# = = = = = = = = = = = = = = = = = = = =
+#      Managing: Envs & File System
+# = = = = = = = = = = = = = = = = = = = =
+
+establish_temp_path() {
+	# Prepare a directory in '/tmp' for temporary files
+	# of the current session and add that path to the
+	# PATH variable.
+	# Returns(by print):
+	#   The valid path of the temporary directory.
+
+
+	local temp_path="/tmp/bash-crud"
+
+	if [ ! -d "$temp_path" ]; then
+		mkdir -p "$temp_path"
+	fi
+
+	if [[ ! "$PATH" =~ $temp_path ]]; then
+		PATH="$PATH:$temp_path"
+	fi
+
+	printf %s "$temp_path"
+}
+
+establish_gawk_path() {
+	# Prepare a child directory for gawk programs
+	# according to a value of the AWKPATH variable.
+	# Conditions:
+	# 	If AWKPATH has multiple paths then the last one is used.
+	# 	If AWKPATH is unset then 'default_awkpath' is used.
+	# Returns(by print):
+	#		A directory path for gawk programs.
+
+
+	local default_awkpath="/usr/share/awk"
+
+	local awk_path
+	awk_path="$(gawk 'BEGIN { len=split(ENVIRON["AWKPATH"], arr, ":"); printf "%s", arr[len] }')"
+	awk_path="${awk_path:-$default_awkpath}"
+
+	local path="${awk_path}/bash-crud"
+	mkdir -p "$path"
+	printf %s "$path"
+}
+
+# get_bin_dir() {
+# 	if [ -n "${BASH_CRUD_BIN_DIR:-}" ]; then
+# 		printf %s "$BASH_CRUD_BIN_DIR"
+# 	else
+# 		printf "/usr/local/bin"
+# 	fi
+# }
+
+
+# = = = = = = = = =
+#  Download links
+# = = = = = = = = =
+
 get_download_link() {
 	local resource="$1"
 	local version_tag; version_tag="${BASH_CRUD_INSTALL_VERSION:-$(get_current_version_tag)}"
@@ -281,10 +328,6 @@ get_download_link() {
 	else
 		print_error_and_exit "Couldn't find a download link for resource type '${resource}'."
 	fi
-}
-
-get_gawk_download_sources() {
-	echo 1
 }
 
 install() {
--- test/install.bats
+++ test/install.bats
@@ -12,6 +12,11 @@ teardown() {
 	rm -rf "${BC_TMP_DIR}/*" "${BC_TMP_DIR}/.*"
 }
 
+
+# = = = = = = = = = =
+#     Utilities
+# = = = = = = = = = =
+
 @test "should print in STDERR: print_error()" {
 	local message="ERROR MESSAGE TO PRINT"
 
@@ -30,20 +35,149 @@ teardown() {
 	[ "$(cat "$BC_DRAFT")" == "$message" ]
 }
 
-@test "should return a valid semver tag starting from 'v' character: get_current_version_tag()" {
+@test "should return a valid semver tag starting with 'v' character: get_current_version_tag()" {
 	SEMVER_REGEX="^v(0|[1-9][0-9]*)\.(0|[1-9][0-9]*)\.(0|[1-9][0-9]*)(\-[0-9A-Za-z-]+(\.[0-9A-Za-z-]+)*)?(\+[0-9A-Za-z-]+(\.[0-9A-Za-z-]+)*)?$"
 	run get_current_version_tag
 	[[ "$output" =~ $SEMVER_REGEX ]]
 }
 
-@test "should check if a command is available: has_cmd()" {
+@test "should check if a string is included in a list: includes_by_delimiter()" {
+	# Case 1
+	local list="A B C D EFG"
+
+	run includes_by_delimiter "$list" " " "A"
+	[ "$status" -eq 0 ]
+
+	run includes_by_delimiter "$list" " " "H"
+	[ "$status" -eq 1 ]
+
+	run includes_by_delimiter "$list" " " "G"
+	[ "$status" -eq 1 ]
+
+	run includes_by_delimiter "$list" " " "EFG"
+	[ "$status" -eq 0 ]
+
+
+	# Case 2
+	list="A*B*C*D*EFG"
+
+	run includes_by_delimiter "$list" "*" "A"
+	[ "$status" -eq 0 ]
+
+	run includes_by_delimiter "$list" "*" "H"
+	[ "$status" -eq 1 ]
+
+	run includes_by_delimiter "$list" "*" "G"
+	[ "$status" -eq 1 ]
+
+	run includes_by_delimiter "$list" "*" "EFG"
+	[ "$status" -eq 0 ]
+
+
+	# Case 3
+	run includes_by_delimiter "A" " " "A"
+	[ "$status" -eq 0 ]
+
+	run includes_by_delimiter "A" "*" "A"
+	[ "$status" -eq 0 ]
+
+	run includes_by_delimiter "" " " "A"
+	[ "$status" -eq 1 ]
+
+	run includes_by_delimiter "" " " ""
+	[ "$status" -eq 0 ]
+
+	run includes_by_delimiter "A B C*D E F" "*" "D E F"
+	[ "$status" -eq 0 ]
+}
+
+@test "should check if a command exists: has_cmd()" {
+	local fake_cmd="BC-not-even-a-command"
+
 	run has_cmd "bash"
 	[ "$status" -eq 0 ]
 
-	run has_cmd "BC-not-even-a-command"
+	run has_cmd "$fake_cmd"
 	[ "$status" -eq 1 ]
+
+	# TO-DO - multiple pass/miss commands
+	export BC_TEST__MISS_COMMAND="bash"
+	run has_cmd "bash"
+	[ "$status" -eq 1 ]
+	unset BC_TEST__MISS_COMMAND
+
+	export BC_TEST__PASS_COMMAND="$fake_cmd"
+	run has_cmd "$fake_cmd"
+	[ "$status" -eq 0 ]
+	unset BC_TEST__PASS_COMMAND
 }
 
-@test "should download files with 'curl' and 'wget': download_file()" {
 
+# = = = = = = = = = = = = = = = =
+#    Managing: Required tools
+# = = = = = = = = = = = = = = = =
+
+@test "should exit successfully: get_architecture_for_jq()" {
+	run get_architecture_for_jq
+	[ "$status" -eq 0 ]
 }
+
+@test "should install the tool: establish_jq()" {
+	# Case 1
+	export BC_TEST__PASS_COMMAND="jq"
+	run establish_jq
+	[ "$status" -eq 0 ]
+	unset BC_TEST__PASS_COMMAND
+
+
+	# Case 2
+	export BC_TEST__MISS_COMMAND="jq"
+
+	local temp_path
+	temp_path="$(establish_temp_path)"
+
+	run establish_jq
+	[ "$status" -eq 0 ]
+	[[ -f "${temp_path}/jq" && -x "${temp_path}/jq" ]]
+
+	run "${temp_path} --help"
+	[ "$status" -eq 0 ]
+
+	unset BC_TEST__MISS_COMMAND
+}
+
+@test "should return a proper downloader command: get_downloader()" {
+	# Case 1
+	for cmd in "curl" "wget"; do
+		printf %s "Item '${cmd}': " >&3
+
+		export BASH_CRUD_DOWNLOADER="$cmd"
+
+		export BC_TEST__PASS_COMMAND="$cmd"
+		run get_downloader
+		[[ "$status" -eq 0 && "$output" == "$cmd" ]]
+		unset BC_TEST__PASS_COMMAND
+
+		export BC_TEST__MISS_COMMAND="$cmd"
+		run get_downloader
+		[ "$status" -eq 1 ]
+		unset BC_TEST__MISS_COMMAND
+
+		printf "%s\n" "OK" >&3
+	done
+
+	unset BASH_CRUD_DOWNLOADER
+
+
+	# Case 2
+	# TO-DO - `print_error_and_exit "Neither 'curl' nor 'wget' commands were found."`
+}
+
+
+# = = = = = = = = = = = =
+#    Networking: Http
+# = = = = = = = = = = = =
+
+# @test "should download files with 'curl' and 'wget': download_file()" {
+
+# }
